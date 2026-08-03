0bfa659a85032625c4ebc1769da2ed1f53501efc
florintours2026-blip <florintours2026@gmail.com>
2026-08-03 07:47:46 +0300

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,29 @@
+// Firebase Configuration
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDN8E8R9GdvQCFxkvYJRNCAwZlDGKd5icg",
+
+    authDomain: "florin-travel.firebaseapp.com",
+
+    projectId: "florin-travel",
+
+    storageBucket: "florin-travel.firebasestorage.app",
+
+    messagingSenderId: "105528948337",
+
+    appId: "1:105528948337:web:563b8e7bea6468af6e0dd0",
+
+    measurementId: "G-6DVY7BXYDW"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
